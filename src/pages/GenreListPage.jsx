8950ab1838cb48cc8825aeb68c8f4927afedb749
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-24 21:20:20 -0300

    Adição da barra de busca

--- src/pages/GenreListPage.jsx
+++ src/pages/GenreListPage.jsx
@@ -1,7 +1,10 @@
+import GenreCard from "../components/gerneCard/GenreCard";
+
 export default function GenreListPage() {
   return (
     <div className="container">
-      <h1>Genre Page</h1>
+      <h1 className="titulo-pagina">Genre Page</h1>
+      <GenreCard />
       {/*Lista todos os gêneros disponíveis em um formato de card,
         cada um com uma cor de fundo diferente, com um link 
         para uma página que exibe filmes pertencentes a esse gênero. */}
--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,38 @@
+import CardContainer from "../components/cardContainer/CardContainer";
+import MovieCard from "../components/movieCard/MovieCard";
+
+import { FaSearch } from "react-icons/fa";
+
+import { useState } from "react";
+import movies from "../data/movies.json";
+
 export default function MovieListPage() {
+  const [listMovies, setListMovies] = useState(movies);
+  const [search, setSearch] = useState("");
+
   return (
     <div className="container">
-      <h1>Movie List Page</h1>
-      {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
+      <h1 className="titulo-pagina">Filmes</h1>
+      <div className="search-bar">
+        <div>
+          <FaSearch />
+        </div>
+        <input
+          type="text"
+          placeholder="Digite o filme:"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+      </div>
+      <CardContainer>
+        {listMovies
+          .filter(movie => movie.titulo.includes(search))
+          .map((movie) => (
+            <MovieCard key={movie.id} {...movie} />
+          ))}
+      </CardContainer>
     </div>
   );
 }
